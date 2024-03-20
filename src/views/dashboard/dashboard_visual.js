737f40cfc85cd214499be7f70cd92f44e134d64f
Tulasi Ram <tulasi_ram.thota@insightsoftware.com>
2024-03-20 14:29:02 +0530

    Dasboard visual changes

--- src/views/dashboard/dashboard_visual.js
+++ src/views/dashboard/dashboard_visual.js
@@ -25,54 +25,31 @@ import {
 import { useNavigate } from "react-router-dom";
 // core components
 import Header from "components/Headers/Header.js";
-import mainjs from "./main.js"
+
 
 const DashboardVisual = () => {
-  const [data, setData] = useState([]);
-
+  const [src, setSrc] = useState(process.env.REACT_APP_COMPOSER_URL+"/visualization/visual-65f81adcd4b65d67150845ce");
 
   useEffect(() => {
-
-    // let maindiv = document.getElementById('main-id');
-    // let mainscript = document.createElement('script');
-    // mainscript.src = `${mainjs}`;    
-    // mainscript.async = true;
-    // maindiv.append(mainscript);
-
-    // let widgetWrapper = document.getElementById('widget-loader-id');
-    // let script = document.createElement('script');
-    // script.src = `http://localhost:8080/composer/embed/embed.js`;
-    // script.dataset.name = 'composer-embed';
-    // script.innerHTML = JSON.stringify({
-    //         type: 'dashboard',
-    //         "dashboardId":"65f81aead4b65d67150845d0"
-    // });
-    // script.async = true;
-    // widgetWrapper.append(script);
-
-    return () => {
-      // Cleanup function to remove the script if necessary
-     // document.head.removeChild(script);
-    };
-  }, []);
+     const dynamicSrc2 = process.env.REACT_APP_COMPOSER_URL+"/visualization/visual-65f81adcd4b65d67150845ce";
+     setSrc(dynamicSrc2);
+  }, []); 
 
   return (
     <>
-     
-      <head>
-       <meta charset="UTF-8"/>
-       <title>Composer Embed</title>
-       <script data-name="composer-embed-manager" src="http://localhost:8080/composer/embed/embed.js"></script>
-      </head>
-      <body>
           <Header />
           <Container className="mt--7" fluid>
-                <div class="widget-holder"></div>
-                <div class="message-holder"></div>
-          </Container>
-          {/* <script src="./main.js"></script> */}
-          <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-       </body>
+          <iframe
+            src={src}
+            width="1400"
+            height="600"
+            title="Embedded Content"
+            frameBorder="0"   
+            style={{paddingTop:46}}   
+            referrerPolicy="no-referrer-when-downgrade"     
+          />
+          </Container>       
+
     </>
   );
 };
